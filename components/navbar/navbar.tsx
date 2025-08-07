1b2cb23c054856bf8ba1361ab611aa54d8c33035
laban405 <laban405@gmail.com>
2025-08-07 14:06:49 +0300

    layout

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -6,28 +6,30 @@ import ThemeToggle from "../shared/theme-toggle";
 import Logo from "../shared/Logo";
 import { auth, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { getUserById } from "@/lib/actions/user.actions";
+import Link from "next/link";
 
 const Navbar = async () => {
-   const { userId } = auth();
-    const user =userId ? await getUserById(userId): null;
+  const { userId } = auth();
+  const user = userId ? await getUserById(userId) : null;
   return (
-    <nav className="fixed z-10 top-6 inset-x-4 h-14 xs:h-16 bg-background/50 backdrop-blur-sm border dark:border-slate-700/70 max-w-screen-xl mx-auto rounded-full">
+    <nav className="fixed z-10 top-6 inset-x-4 h-14 xs:h-16 bg-background/50 backdrop-blur-sm border  max-w-screen-xl mx-auto rounded-full">
       <div className="h-full flex items-center justify-between mx-auto px-4">
-       <Logo height={20} width={32}/>
+        <Logo height={20} width={32} />
 
         {/* Desktop Menu */}
         <NavMenu className="hidden md:block" />
 
         <div className="flex items-center gap-3">
           <SignedIn>
-           <UserButton afterSignOutUrl="/" showName />
-           </SignedIn>
-         <SignedOut>
-          <Button variant="outline" className="hidden sm:inline-flex">
-            Sign In
-          </Button><Button className="hidden xs:inline-flex">Get Started</Button>
+            <UserButton afterSignOutUrl="/" showName />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild variant="outline" className="hidden sm:inline-flex">
+              <Link href="/sign-in">Sign In</Link>
+            </Button>
+            <Button className="hidden xs:inline-flex">Get Started</Button>
           </SignedOut>
-         {/* <ThemeToggle /> */}
+          {/* <ThemeToggle /> */}
 
           {/* Mobile Menu */}
           <div className="md:hidden">
