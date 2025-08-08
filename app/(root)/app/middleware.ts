092dd0cc370e6ade8ffd66249958518c7ef8b017
laban405 <laban405@gmail.com>
2025-08-08 20:20:00 +0300

    styling

--- app/(root)/app/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-export default authMiddleware({
-  publicRoutes: ['/',"/app", '/api/webhooks/clerk', '/api/webhooks/stripe',"/api/webhooks/mpesa/stk-push-callback(.*)"]
-});
- 
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/","/app", "/(api|trpc|app)(.*)"],
-};
